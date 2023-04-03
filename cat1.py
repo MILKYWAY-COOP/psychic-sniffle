8e2c15e00f686279d85169341441f1b6056f0136
Stephen Muchendu <muchendumaina66@gmail.com>
2023-04-03 22:01:00 +0300

    practice

---
+++ cat1.py
@@ -0,0 +1,8 @@
+# Python program that calculates the area of a rectangle, circle, triangle or trapezium
+def calculateArea():
+    print("Please select a shape: \nA. Circle\nB. Rectangle\nC. Triangle\nD. Trapezium\n")
+    shape = input()
+    if shape == "A":
+        print()
+
+calculateArea()
